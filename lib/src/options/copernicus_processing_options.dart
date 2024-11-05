486b345852bdf0df834881e3a23eee550c52b479
danemadsen <dane_madsen@hotmail.com>
2024-11-05 13:32:12 +1000

    docs

--- lib/src/options/copernicus_processing_options.dart
+++ lib/src/options/copernicus_processing_options.dart
@@ -4,17 +4,87 @@ import 'package:copernicus/src/types/copernicus_dem_instance.dart';
 import 'package:copernicus/src/types/copernicus_speckle_filtering.dart';
 import 'package:copernicus/src/types/copernicus_sampling.dart';
 
+/// A class representing the processing options for Copernicus.
+/// 
+/// This class contains various settings and configurations that can be
+/// used to customize the processing behavior of the Copernicus system.
 class CopernicusProcessingOptions {
+  /// The upSampling option for Copernicus processing.
+  /// 
+  /// This option specifies the type of up-sampling to be applied during the 
+  /// processing of Copernicus data. It can be null, indicating that no 
+  /// up-sampling is to be performed.
+  /// 
+  /// Possible values are defined in the [CopernicusSampling] enum.
   final CopernicusSampling? upSampling;
+
+  /// The downSampling option for Copernicus processing.
+  /// 
+  /// This option allows specifying the type of down-sampling to be applied
+  /// during the processing of Copernicus data. It can be null, indicating
+  /// that no down-sampling should be performed.
+  /// 
+  /// Possible values are defined in the [CopernicusSampling] enum.
   final CopernicusSampling? downSampling;
+
+  /// The backscattering coefficient option for Copernicus processing.
+  /// 
+  /// This parameter is optional and can be null. It represents the 
+  /// backscattering coefficient used in the processing of Copernicus data.
   final CopernicusBackCoefficiency? backCoefficiency;
+
+  /// Indicates whether orthorectification should be applied.
+  /// 
+  /// Orthorectification is the process of removing geometric distortions
+  /// from imagery so that the scale is uniform and the image can be used
+  /// as a map. If `true`, orthorectification will be applied. If `false`
+  /// or `null`, orthorectification will not be applied.
   final bool? orthorectify;
+
+  /// The digital elevation model (DEM) instance used for processing.
+  /// 
+  /// This can be `null` if no DEM instance is specified.
   final CopernicusDemInstance? demInstance;
+
+  /// The level of oversampling applied to radiometric terrain processing.
+  /// 
+  /// This value can be `null`, indicating that no oversampling is applied.
+  /// Higher values indicate more oversampling, which can improve the quality
+  /// of the processed terrain data at the cost of increased processing time.
   final int? radiometricTerrainOversampling;
+
+  /// The speckle filtering options for Copernicus processing.
+  /// 
+  /// This parameter allows you to specify the type of speckle filtering to be applied
+  /// during the processing of Copernicus data. Speckle filtering is used to reduce
+  /// noise in radar images.
+  /// 
+  /// It can be `null` if no speckle filtering is to be applied.
   final CopernicusSpeckleFiltering? speckleFiltering;
+
+  /// The minimum quality assurance (QA) value.
+  /// 
+  /// This value is optional and can be null. If provided, it represents the 
+  /// minimum acceptable QA value for processing.
   final int? minQa;
+
+  /// Indicates whether the Earth Gravitational Model (EGM) is used.
+  /// 
+  /// If `true`, the EGM is applied. If `false`, the EGM is not applied.
+  /// If `null`, the default behavior is used.
   final bool? egm;
 
+  /// Constructs an instance of [CopernicusProcessingOptions].
+  ///
+  /// This class holds the processing options for the Copernicus library.
+  /// 
+  /// Example usage:
+  /// 
+  /// ```dart
+  /// final options = CopernicusProcessingOptions(
+  ///   // provide necessary parameters here
+  /// );
+  /// ```
   const CopernicusProcessingOptions({
     this.upSampling, 
     this.downSampling,
@@ -27,6 +97,11 @@ class CopernicusProcessingOptions {
     this.egm,
   });
 
+  /// Returns a map representation of the processing options.
+  ///
+  /// The map contains key-value pairs where the keys are strings
+  /// representing the option names and the values are dynamic
+  /// representing the option values.
   Map<String, dynamic> get map {
     Map<String, dynamic> map = {};
 
@@ -69,6 +144,13 @@ class CopernicusProcessingOptions {
     return map;
   }
 
+  /// Creates an instance of [CopernicusProcessingOptions] from a map.
+  ///
+  /// The [map] parameter is a [Map] containing key-value pairs that correspond
+  /// to the properties of [CopernicusProcessingOptions].
+  ///
+  /// Returns an instance of [CopernicusProcessingOptions] populated with the
+  /// values from the provided [map].
   factory CopernicusProcessingOptions.fromMap(Map<String, dynamic> map) {
     CopernicusSampling? upSampling;
 
@@ -140,6 +222,14 @@ class CopernicusProcessingOptions {
     );
   }
 
+  /// Validates the given Copernicus satellite.
+  ///
+  /// This method checks the provided [satillite] to ensure it meets the
+  /// necessary criteria for processing.
+  ///
+  /// Throws an [ArgumentError] if the satellite is not valid.
+  ///
+  /// - Parameter satillite: The Copernicus satellite to be validated.
   void validate(CopernicusSatillite satillite) {
     List<String> errors = [];
 
