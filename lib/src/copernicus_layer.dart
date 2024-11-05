ba589ae80e4d03b7404c9ea38727066e2ad4173f
danemadsen <dane_madsen@hotmail.com>
2024-11-05 13:18:40 +1000

    docs

--- lib/src/copernicus_layer.dart
+++ lib/src/copernicus_layer.dart
@@ -7,14 +7,56 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:copernicus/src/providers/copernicus_tile_provider.dart';
 
-
+/// A widget that represents a Copernicus layer.
+///
+/// This widget is used to display data from the Copernicus satellite.
+/// It requires a client ID and client secret for authentication, and a list
+/// of request data to specify what data to fetch. Optionally, a Dio client
+/// can be provided for making HTTP requests, and a map of bands with their
+/// corresponding values can be specified.
+///
+/// The default bands are:
+/// - `CopernicusBand.s2b04` with a value of 2.5
+/// - `CopernicusBand.s2b03` with a value of 2.5
+/// - `CopernicusBand.s2b02` with a value of 2.5
+///
+/// The `CopernicusLayer` widget is a stateful widget, and its state is managed
+/// by the `_CopernicusLayerState` class.
+///
+/// Parameters:
+/// - `dioClient`: An optional Dio client for making HTTP requests.
+/// - `clientId`: The client ID for authentication.
+/// - `clientSecret`: The client secret for authentication.
+/// - `data`: A list of request data specifying what data to fetch.
+/// - `bands`: An optional map of bands with their corresponding values.
 class CopernicusLayer extends StatefulWidget {
+  /// An optional instance of the Dio HTTP client.
+  /// This can be used to make network requests within the Copernicus layer.
   final Dio? dioClient;
+
+  /// The client ID used to authenticate requests.
+  /// 
+  /// This is a unique identifier assigned to each client application
+  /// that is used to track and manage API usage.
   final String clientId;
+
+  /// The secret key used to authenticate the client.
+  /// 
+  /// This key should be kept confidential and not exposed in the source code.
+  /// It is used to securely communicate with the server.
   final String clientSecret;
+
+  /// A list of [CopernicusRequestData] objects representing the data for the Copernicus layer.
   final List<CopernicusRequestData> data;
+
+  /// A map that associates each [CopernicusBand] with a corresponding double value.
+  /// This can be used to store and retrieve band-specific data for a Copernicus layer.
   final Map<CopernicusBand, double> bands;
   
+  /// Creates a new instance of the [CopernicusLayer].
+  ///
+  /// This constructor initializes the [CopernicusLayer] with the provided
+  /// parameters.
   const CopernicusLayer({
     super.key, 
     this.dioClient,
